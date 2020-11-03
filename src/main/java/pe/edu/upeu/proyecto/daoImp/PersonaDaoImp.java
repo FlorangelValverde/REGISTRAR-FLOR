2bb5cd96d1ff34d08b2079860030c99870ea716c
Diego Alor <diegoalor@upeu.edu.pe>
2020-11-03 09:37:21 -0500

    Implement

--- src/main/java/pe/edu/upeu/proyecto/daoImp/PersonaDaoImp.java
+++ src/main/java/pe/edu/upeu/proyecto/daoImp/PersonaDaoImp.java
@@ -2,30 +2,41 @@ package pe.edu.upeu.proyecto.daoImp;
 
 import java.util.Map;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.stereotype.Component;
 
 import pe.edu.upeu.proyecto.dao.PersonaDao;
 import pe.edu.upeu.proyecto.entity.Persona;
 
-@Repository
+@Component
 public class PersonaDaoImp implements PersonaDao {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	private SimpleJdbcCall simpleJdbcCall;
+	
 	@Override
 	public int create(Persona persona) {
 		// TODO Auto-generated method stub
-		return 0;
+		return jdbcTemplate.update("call PKG_PERSONA.PR_INS_PERSONA(?,?,?,?,?,?,?,?,?)", persona.getNombre(), 
+									persona.getApellidos(), persona.getDni(), persona.getFec_nacimiento(), persona.getCelular(),
+									persona.getCorreo(), persona.getUsuario(), persona.getClave(), persona.getIdestado_civil());
 	}
 
 	@Override
 	public int update(Persona persona) {
 		// TODO Auto-generated method stub
-		return 0;
+		return jdbcTemplate.update("call PKG_PERSONA.PR_UPD_PERSONA(?,?,?,?,?,?,?,?,?,?)", persona.getIdpersona(), persona.getNombre(), 
+				persona.getApellidos(), persona.getDni(), persona.getFec_nacimiento(), persona.getCelular(),
+				persona.getCorreo(), persona.getUsuario(), persona.getClave(), persona.getIdestado_civil());
 	}
 
 	@Override
 	public int delete(int id) {
 		// TODO Auto-generated method stub
-		return 0;
+		return jdbcTemplate.update("call PKG_PERSONA.PR_DEL_PERSONA(?)", id);
 	}
 
 	@Override
